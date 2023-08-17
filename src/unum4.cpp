7cce0e4313a13270d0a00c1996aeaea2d412e0c2
Joao D Lopes <joao.d.lopes91@gmail.com>
2023-08-17 19:04:04 +0100

    add input and output operators; organize code for faster understanding

--- src/unum4.cpp
+++ src/unum4.cpp
@@ -7,15 +7,24 @@ uint8_t failed, overflow, underflow, div_by_zero;
 
 class Unum4 {
     private:
-        unum4 val = 0;
+        unum4 val;
     public:
-        Unum4(): val() {};
+
+        //
+        // Declarations
+        //
+
+        Unum4(): val(float2unum4(0.0, &failed)) {};
         Unum4(float f): val(float2unum4(f, &failed)) {};
         Unum4(double d): val(double2unum4(d, &failed)) {};
         Unum4(int i): val(double2unum4((double)i, &failed)) {};
 
+        //
         // Arithmetic operators
-        Unum4 operator+(Unum4 const& other) const {
+        //
+
+        // Addition
+        Unum4 operator+(Unum4 const& other) {
             Unum4 res;
             res.val = unum4_add(val, other.val, &overflow);
             return res;
@@ -25,7 +34,8 @@ class Unum4 {
             return val = unum4_add(val, other.val, &overflow);
         }
 
-        Unum4 operator-(Unum4 const& other) const {
+        // Subtraction
+        Unum4 operator-(Unum4 const& other) {
             Unum4 res;
             res.val = unum4_sub(val, other.val, &overflow);
             return res;
@@ -35,7 +45,8 @@ class Unum4 {
             return val = unum4_sub(val, other.val, &overflow);
         }
 
-        Unum4 operator*(Unum4 const& other) const {
+        // Multiplication
+        Unum4 operator*(Unum4 const& other) {
             Unum4 res;
             res.val = unum4_mul(val, other.val, &overflow, &underflow);
             return res;
@@ -45,7 +56,8 @@ class Unum4 {
             return val = unum4_mul(val, other.val, &overflow, &underflow);
         }
 
-        Unum4 operator/(Unum4 const& other) const {
+        // Division
+        Unum4 operator/(Unum4 const& other) {
             Unum4 res;
             res.val = unum4_div(val, other.val, &overflow, &underflow, &div_by_zero);
             return res;
@@ -55,7 +67,10 @@ class Unum4 {
             return val = unum4_div(val, other.val, &overflow, &underflow, &div_by_zero);
         }
 
+        //
         // Comparator operators
+        //
+
         bool operator<(Unum4 const& other) { return (bool)unum4_lt(val, other.val); }
         bool operator<=(Unum4 const& other) { return (bool)unum4_le(val, other.val); }
         bool operator>(Unum4 const& other) { return (bool)unum4_gt(val, other.val); }
@@ -63,44 +78,74 @@ class Unum4 {
         bool operator==(Unum4 const& other) { return (bool)unum4_eq(val, other.val); }
         bool operator!=(Unum4 const& other) { return (bool)!unum4_eq(val, other.val); }
 
+        //
         // Conversion operators
+        //
+
         operator float() { return unum42float(val); }
         operator double() { return unum42double(val); }
         operator int() { return (int)unum42double(val); }
+
+        //
+        // Input/Output
+        //
+
+        friend std::ostream &operator<<(std::ostream &output, const Unum4 &other ) {
+            output << unum42double(other.val);
+         return output;
+        }
+
+        friend std::istream &operator>>(std::istream  &input, Unum4 &other ) {
+            double value;
+            input >> value;
+            other.val = double2unum4(value, &failed);
+            return input;
+        }
 };
 
 int main(int argc, char **argv) {
     Unum4 a = 1.0;
     Unum4 b = 2.0;
+    Unum4 c;
 
     // Arithmetics
     std::cout << std::endl << "Arithmetic tests" << std::endl << std::endl;
-    std::cout << "a = " << (double)a << std::endl;
-    std::cout << "b = " << (double)b << std::endl;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
     std::cout << std::endl;
-    Unum4 c = a + b;
-    std::cout << "c = a + b = " << (double)c << std::endl;
+
+    c = a + b;
+    std::cout << "c = a + b = " << c << std::endl;
     c += b;
-    std::cout << "c += b = " << (double)c << std::endl;
+    std::cout << "c += b = " << c << std::endl;
+    std::cout << std::endl;
+
     c = a - b;
-    std::cout << "c = a - b = " << (double)c << std::endl;
+    std::cout << "c = a - b = " << c << std::endl;
     c -= b;
-    std::cout << "c -= b = " << (double)c << std::endl;
+    std::cout << "c -= b = " << c << std::endl;
+    std::cout << std::endl;
+
     c = a * b;
-    std::cout << "c = a * b = " << (double)c << std::endl;
+    std::cout << "c = a * b = " << c << std::endl;
     c *= b;
-    std::cout << "c *= b = " << (double)c << std::endl;
+    std::cout << "c *= b = " << c << std::endl;
+    std::cout << std::endl;
+
     c = a / b;
-    std::cout << "c = a / b = " << (double)c << std::endl;
+    std::cout << "c = a / b = " << c << std::endl;
     c /= b;
-    std::cout << "c /= b = " << (double)c << std::endl;
+    std::cout << "c /= b = " << c << std::endl;
     std::cout << std::endl;
 
     // Comparators
     std::cout << std::endl << "Comparator tests" << std::endl << std::endl;
-    std::cout << "a = " << (double)a << std::endl;
-    std::cout << "b = " << (double)b << std::endl;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
     std::cout << std::endl;
+
     if (a > b) {
       std::cout << "a > b" << std::endl;
     } else {
@@ -135,9 +180,10 @@ int main(int argc, char **argv) {
 
     a = 2.0;
     b = 1.0;
-    std::cout << "a = " << (double)a << std::endl;
-    std::cout << "b = " << (double)b << std::endl;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
     std::cout << std::endl;
+
     if (a > b) {
       std::cout << "a > b" << std::endl;
     } else {
@@ -172,9 +218,10 @@ int main(int argc, char **argv) {
 
     a = 2.0;
     b = 2.0;
-    std::cout << "a = " << (double)a << std::endl;
-    std::cout << "b = " << (double)b << std::endl;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
     std::cout << std::endl;
+
     if (a > b) {
       std::cout << "a > b" << std::endl;
     } else {
@@ -209,8 +256,10 @@ int main(int argc, char **argv) {
 
     // Conversions
     std::cout << std::endl << "Arithmetic tests" << std::endl << std::endl;
+
     c = 37.48;
-    std::cout << "c = " << (double)c << std::endl;
+    std::cout << "c = " << c << std::endl;
+
     std::cout << "(double)c = " << (double)c << std::endl;
     std::cout << "(float)c = " << (float)c << std::endl;
     std::cout << "(int)c = " << (int)c << std::endl;
